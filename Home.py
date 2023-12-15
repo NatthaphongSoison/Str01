40919f03af6fd6a829573364897b072b1f56e78b
NatthaphongSoison <153786702+NatthaphongSoison@users.noreply.github.com>
2023-12-15 14:27:14 +0700

    commit

--- Home.py
+++ Home.py
@@ -5,3 +5,11 @@ st.title('การทดสอบเขียนเว็บด้วย Python
 st.header('ณัฐพงษ์ สร้อยสน ')
 st.subheader('สาขาเทคโนโลยีสารสนเทศ')
 st.markdown("----")
+
+col1, col2 = st.columns(2)
+#col1.write("This is column 1")
+#col2.write("This is column 2")
+with col1:
+    st.image('./pic/12.jpg')
+with col2:
+    st.image('./pic/13.jpg')
