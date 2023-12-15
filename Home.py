5b45a0f54233ee5f57198a3f5270202846b211f8
NatthaphongSoison <153786702+NatthaphongSoison@users.noreply.github.com>
2023-12-15 14:21:12 +0700

    .commit

--- Home.py
+++ Home.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.header("Natthaphong Soison")
+st.title('การทดสอบเขียนเว็บด้วย Python')
+st.header('ณัฐพงษ์ สร้อยสน ')
+st.subheader('สาขาเทคโนโลยีสารสนเทศ')
+st.markdown("----")
