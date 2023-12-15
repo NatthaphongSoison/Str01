e696050be61c7aa8b821a1fd1a76d1503b7dacbb
NatthaphongSoison <153786702+NatthaphongSoison@users.noreply.github.com>
2023-12-15 14:35:00 +0700

    commit

--- Home.py
+++ Home.py
@@ -13,3 +13,10 @@ with col1:
     st.image('./pic/12.jpg')
 with col2:
     st.image('./pic/13.jpg')
+html_1 = """
+<div style="background-color:#52BE80;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>สถิติข้อมูลดอกไม้</h5></center>
+</div>
+"""
+st.markdown(html_1, unsafe_allow_html=True)
+st.markdown("")
